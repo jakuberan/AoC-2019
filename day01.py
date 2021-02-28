d476ebfb355a6c012cc58ee32abede35bc1efe1b
Jakub Beran <nomail>
2021-02-28 21:12:18 +0100

    day01.py: Simple solution

--- day01.py
+++ day01.py
@@ -2,8 +2,16 @@
 data_path = "data/day01"
 
 # Read line-by-line
-list_of_sums = []
+fuel_total_1 = 0
+fuel_total_2 = 0
 f = open(data_path, "r")
 for x in f:
     # Convert to int and append
-    list_of_sums.append(int(x))
+    fuel = int(int(x) / 3) - 2
+    fuel_total_1 += fuel
+    while fuel > 0:
+        fuel_total_2 += fuel
+        fuel = int(fuel / 3) - 2
+    
+print("Total fuel needed (part 1): " + str(fuel_total_1))
+print("Total fuel needed (part 2): " + str(fuel_total_2)) 
